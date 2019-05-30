1bab291192e19b98d23a5512417178695284d49f
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-05-30 18:39:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,5 +3,9 @@ def fizzbuzz(number)
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-    elsif 
+  elsif number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
+  else 
+    return nil
+  end
 end
